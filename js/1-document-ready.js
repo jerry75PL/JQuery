7c9fcfd7d6682a80e6acee63c00e1d0eba56a2ad
jteklak <jteklak@gmail.com>
2017-06-24 14:32:08 +0200

    1 and 2

---
+++ js/1-document-ready.js
@@ -0,0 +1,11 @@
+'use strict';
+
+$(function () {
+
+
+
+    $('#first').css({color: 'red'}).hide(2000).show(3000);
+
+
+});
+
---
+++ js/2-selektory.js
@@ -0,0 +1,12 @@
+'use strict';
+
+$(function () {
+    var paragraphs = $('p');
+    console.log(paragraphs);
+    
+    var paragraphOne = $('#paragraf')
+    console.log(paragraphOne);
+    
+    var paragraphTwo = $('.paragraf');
+    console.log(paragraphTwo);
+});
