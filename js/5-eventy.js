69ccfbd59422b969e3c9b38247fb514015dc7381
jteklak <jteklak@gmail.com>
2017-06-24 15:41:18 +0200

    eventy

--- js/5-eventy.js
+++ js/5-eventy.js
@@ -2,10 +2,26 @@
 
 $(function () {
 
+    $('p').click(function () {
+        $(this).css('color', 'orange');
 
 
-    
+    });
+
+
+    $('h1').on({
+
+        'click': function () {
+            $(this).css('color', 'orange');
+        },
+        'mouseleave': function () {
+            $(this).css('color', 'inherit');
+        }
+
+
+    });
+
+
 
 
 });
-
