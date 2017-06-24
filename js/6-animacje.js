346c29bfbea4f484b26ebc74d7e7daf4677685c4
jteklak <jteklak@gmail.com>
2017-06-24 16:10:42 +0200

    animacje

--- js/6-animacje.js
+++ js/6-animacje.js
@@ -2,8 +2,21 @@
 
 $(function () {
 
+    $('p').hide(3000).show(3000);
+    
+    $('h1').fadeOut(3000).fadeIn(3000);
+    
+    $('h2').slideUp(3000).slideDown(3000);
 
 
+    function animacja(){
+        
+        console.log('zakończono pierwszą animację. Zaraz startuje druga :)');
+        
+        $('h3').animate({'font-size' : '1em', 'margin-left' : '0px'}, 2000);
+    }
+    
+    $('h3').animate({'font-size' : '3em', 'margin-left' : '100px'}, 2000, animacja);
    
 
 
